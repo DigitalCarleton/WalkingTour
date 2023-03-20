21fed2b8ecad9962f211a41169d50160d768de49
tutingjun <tut@carleton.edu>
2023-03-20 05:37:18 -0500

    replacing earlier tile swap for esri over stamen

--- views/public/javascripts/mall-map.js
+++ views/public/javascripts/mall-map.js
@@ -16,7 +16,7 @@ function mallMapJs(){
     // imported.src = "/cgmrdev/plugins/MallMap/views/public/javascripts/new_markercluster_src.js";
     // document.head.appendChild(imported);
 
-    var MAP_URL_TEMPLATE = 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png';
+    var MAP_URL_TEMPLATE = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
 
     // MAP_CENTER controls the default starting place
     // var MAP_CENTER = [38.8891, -77.02949];
@@ -47,10 +47,10 @@ function mallMapJs(){
     });
     map.addLayer(L.tileLayer(MAP_URL_TEMPLATE));
     // map.addLayer(L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     //             }));
     map.addControl(L.control.zoom({position: 'topleft'}));
-    map.attributionControl.setPrefix('');
+    map.attributionControl.setPrefix('Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community');
 
     // Check for user's first time visiting. Wait to locate the user after displaying tooltip on the first visit.
     if(!($.cookie('myCookie'))) {
